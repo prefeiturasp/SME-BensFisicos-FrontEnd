0a7ed808a8adb848fb57f6d2ebd732451e4f22ae
Bruna Lima <bruna.lima9254@gmail.com>
2026-01-12 10:39:29 -0300

    Adiciona reporter ao coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,5 +12,27 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./src/setupTests.ts'],
     include: ['src/**/*.test.{ts,tsx}'],
+    coverage: {
+      reporter: ["text", "lcov"],
+      reportsDirectory: "./coverage",
+      exclude: [
+        "**/.next/**", // 👈 Exclui TUDO dentro de .next/
+        "src/components/ui/**", // 👈 Exclui apenas a pasta 'ui' dentro de components
+        "src/const.ts",
+        "src/app/api/*",
+        "*/types/*",
+        "next.config.mjs",
+        "tailwind.config.js",
+        "postcss.config.mjs",
+        "src/lib/zod-i18n.ts",
+        "next-env.d.ts",
+        "vitest.config.ts",
+        "eslint.config.mjs",
+        "*/.next/*", // Pode ser redundante, mas não atrapalha
+        "testes/**", // Exclui a pasta de testes de QA
+        "cypress.config.js", // Exclui config do Cypress
+        "cypress/support/**", // Exclui arquivos de suporte do Cypress
+      ],
+    },
   },
 });
